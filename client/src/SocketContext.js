8902a3674f34858e4aab75471e2b9f75704890df
shawmonika616ms <shawmonika616ms@gmail.com>
2024-06-09 18:19:18 +0530

    Description : logic for ansercall, endcall and callUser is completed. Developer : Monika.

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -11,8 +11,13 @@ const ContextProvider = ({ children }) => {
     const [stream, setStream] = useState(null)
     const [me, setMe] = useState('')
     const [call, setCall] = useState({})
+    const [callAccepted, setCallAccepted] = useState(false);
+    const [callEnded, setCallEnded] = useState(false)
+    const [name, setName] = useState('')
 
     const myVideo = useRef();
+    const userVideo = useRef();
+    const connectionRef = useRef();
 
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
@@ -30,14 +35,74 @@ const ContextProvider = ({ children }) => {
 
 
     const anwerCall = () => {
+        setCallAccepted(true)
+        const peer = new Peer({
+            initiator: false,
+            trickle: false,
+            stream: stream
+        })
+
+        peer.on('signal', (data) => {
+            socket.emit('answercall', { signal: data, to: call.from })
+        })
+
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream
+        })
+
+        peer.signal(call.signal)
+
+        connectionRef.current = peer
 
     }
 
-    const callUser = () => {
+    const callUser = (id) => {
+        const peer = new Peer({
+            initiator: true,
+            trickle: false,
+            stream: stream
+        })
 
+        peer.on('signal', (data) => {
+            socket.emit('callUser', { userToCall: id, signalData: data, from: me, name })
+        })
+
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream
+        })
+
+        socket.on('callAccepted', (signal) => {
+            setCallAccepted(true)
+            peer.signal(signal)
+        });
+
+        connectionRef.current = peer
     }
 
     const leaveCall = () => {
+        setCallEnded(true)
+        connectionRef.current.destroy();
 
+        window.location.reload()
     }
+
+    return
+    <SocketContext.Provider value={{
+        call,
+        callAccepted,
+        myVideo,
+        userVideo,
+        stream,
+        name,
+        setName,
+        callEnded,
+        me,
+        callUser,
+        anwerCall,
+        leaveCall,
+    }}>
+        {children}
+    </SocketContext.Provider>
 }
+
+export { ContextProvider, SocketContext }
