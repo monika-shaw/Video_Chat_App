8d101a63d8282e24c22fb680970090c7a5cfa6e7
shawmonika616ms <shawmonika616ms@gmail.com>
2024-06-09 15:23:43 +0530

    Description : created the folder structure and components. Developer : Monika.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { AppBar, Typography } from '@material-ui/core'
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
 function App() {
-  return (
-    <div className="App">
-     Video Chat App
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align="center">
+                    Video Chat
+                </Typography>
+            </AppBar>
+            <VideoPlayer />
+            <Options>
+                <Notifications />
+            </Options>
+        </div>
+    );
 }
 
 export default App;
